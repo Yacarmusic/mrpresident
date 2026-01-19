7b7d78ad44e6e9f0ecd075b84ffdcfb7033cac22
Adrián Yacar López <admin@seacabolafiesta.com>
2026-01-19 12:50:28 +0100

    fix: add missing React import to layout

--- src/app/curso/layout.js
+++ src/app/curso/layout.js
@@ -1,4 +1,5 @@
 "use client";
+import React from 'react';
 import Link from 'next/link';
 import { UserButton, useUser } from '@clerk/nextjs';
 import { CheckCircle2, Circle, Menu, X, Lock } from 'lucide-react';
