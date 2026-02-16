87a7457db8e635fa6414f2830ca58302565e348b
Adrián Yacar López <admin@seacabolafiesta.com>
2026-02-17 00:37:20 +0100

    feat: Inject Clerk user email into Tally survey for vote limitation

--- src/components/TallyEmbed.js
+++ src/components/TallyEmbed.js
@@ -1,12 +1,20 @@
 'use client';
 
 import Script from 'next/script';
+import { useUser } from '@clerk/nextjs';
 
 export default function TallyEmbed() {
+    const { user } = useUser();
+
+    // Tally URL
+    const baseUrl = "https://tally.so/embed/Np6rEO?alignLeft=1&transparentBackground=1&dynamicHeight=1";
+    // If user exists, add email to URL (Hidden Field)
+    const tallySrc = user ? `${baseUrl}&email=${encodeURIComponent(user.primaryEmailAddress.emailAddress)}` : baseUrl;
+
     return (
         <div className="w-full">
             <iframe
-                data-tally-src="https://tally.so/embed/Np6rEO?alignLeft=1&transparentBackground=1&dynamicHeight=1"
+                data-tally-src={tallySrc}
                 loading="lazy"
                 width="100%"
                 height="298"
