42e87329420b645e88e893fef3763778e6732c67
Adrián Yacar López <admin@seacabolafiesta.com>
2026-02-17 00:14:14 +0100

    feat: Add Tally.so survey to course welcome page

--- src/app/curso/page.js
+++ src/app/curso/page.js
@@ -1,6 +1,31 @@
+import Script from 'next/script';
+
 export default function CourseWelcome() {
     return (
         <div className="space-y-6">
+            {/* Tally Survey Embed */}
+            <div className="w-full">
+                <iframe
+                    data-tally-src="https://tally.so/embed/Np6rEO?alignLeft=1&transparentBackground=1&dynamicHeight=1"
+                    loading="lazy"
+                    width="100%"
+                    height="298"
+                    frameBorder="0"
+                    marginHeight="0"
+                    marginWidth="0"
+                    title="Evento Mr. President"
+                ></iframe>
+                <Script
+                    id="tally-js"
+                    src="https://tally.so/widgets/embed.js"
+                    onLoad={() => {
+                        if (typeof Tally !== 'undefined') {
+                            Tally.loadEmbeds();
+                        }
+                    }}
+                />
+            </div>
+
             {/* Important Announcement Banner */}
             <div style={{
                 border: '2px solid #c5a059',
