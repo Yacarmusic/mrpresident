e64942be487a7eccace10e54875b7f31af00fdca
Adrián Yacar López <admin@seacabolafiesta.com>
2026-02-17 00:18:57 +0100

    style: Move Tally survey to bottom of welcome page

--- src/app/curso/page.js
+++ src/app/curso/page.js
@@ -3,9 +3,6 @@ import TallyEmbed from '@/components/TallyEmbed';
 export default function CourseWelcome() {
     return (
         <div className="space-y-6">
-            {/* Tally Survey Embed */}
-            <TallyEmbed />
-
             {/* Important Announcement Banner */}
             <div style={{
                 border: '2px solid #c5a059',
@@ -31,6 +28,11 @@ export default function CourseWelcome() {
 
             <h1 className="text-3xl font-cinzel text-white mb-2">BIENVENIDO AL PODER</h1>
             <p className="text-zinc-400">Selecciona un módulo en el menú de la izquierda para comenzar tu formación.</p>
+
+            {/* Tally Survey Embed */}
+            <div className="mt-8">
+                <TallyEmbed />
+            </div>
         </div>
     );
 }
