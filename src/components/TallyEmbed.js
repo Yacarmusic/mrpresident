674c1ca83c39c755596cb035174d449240380fb0
Adrián Yacar López <admin@seacabolafiesta.com>
2026-02-17 00:16:12 +0100

    fix: Extract TallyEmbed to Client Component to resolve build error

---
+++ src/components/TallyEmbed.js
@@ -0,0 +1,29 @@
+'use client';
+
+import Script from 'next/script';
+
+export default function TallyEmbed() {
+    return (
+        <div className="w-full">
+            <iframe
+                data-tally-src="https://tally.so/embed/Np6rEO?alignLeft=1&transparentBackground=1&dynamicHeight=1"
+                loading="lazy"
+                width="100%"
+                height="298"
+                frameBorder="0"
+                marginHeight="0"
+                marginWidth="0"
+                title="Evento Mr. President"
+            ></iframe>
+            <Script
+                id="tally-js"
+                src="https://tally.so/widgets/embed.js"
+                onLoad={() => {
+                    if (typeof Tally !== 'undefined') {
+                        Tally.loadEmbeds();
+                    }
+                }}
+            />
+        </div>
+    );
+}
